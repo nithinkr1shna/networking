4fd0f1aa29049748869e31df4379e3a9d7a77233
nitkna <nithin.krishna@outlook.com>
2017-08-28 16:45:29 +0530

    adds support for script

---
+++ http_server/handle_scripts.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define OUTPUT_SIZE 9999
+
+char* run_script(char *path){
+    
+    char output[OUTPUT_SIZE]; 
+    int numbytes;
+    int link[2];
+    pid_t pid, status =0 ;
+    char cmd[250];
+    //printf("%s\n", path);
+    //printf("INSIDE RUN_SCRIPT\n");
+
+    if(pipe(link) == -1)
+      printf("error\n");
+    //printf("after pipe\n");
+    
+    if((pid = fork()) == -1)
+      printf("error in fork\n");
+
+    if(pid == 0){
+   	    printf("inside fork\n");
+   	    dup2(link[1], STDOUT_FILENO);
+   	    close(link[0]);
+   	    close(link[1]);
+        //sprintf(cmd, "%s%s","python ", path);
+        //system(cmd);
+        if(execlp("python","python", path, (char*)0) == -1)
+          perror("execl");
+
+
+    }else{
+       
+      close(link[1]);
+    	numbytes = read(link[0], output, sizeof output);
+      //close(link[0]);
+      //printf("Output: (%.*s)\n", numbytes, output);
+      wait(NULL);
+  
+     
+   }
+   
+    
+    return output;
+
+}
---
+++ http_server/header.h
@@ -0,0 +1,1 @@
+char* run_script(char*);
